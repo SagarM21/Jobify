b24260f8e3b407ba858740e51c6079cb5ff0760f
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-03-02 20:55:35 +0530

    Axios Setup

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -34,6 +34,37 @@ const AppContext = React.createContext();
 const AppProvider = ({ children }) => {
 	const [state, dispatch] = useReducer(reducer, initialState);
 
+	// setting axios globally
+	// 1st method->	axios.defaults.headers.common["Authorization"] = `Bearer ${state.token}`;
+	const authFetch = axios.create({
+		baseURL: "/api/v1",
+	});
+
+	//request
+	authFetch.interceptors.request.use(
+		(config) => {
+			//config.headers.common["Authorization"] = `Bearer ${state.token}`;
+			return config;
+		},
+		(error) => {
+			return Promise.reject(error);
+		}
+	);
+
+	//response
+	authFetch.interceptors.response.use(
+		(response) => {
+			return response;
+		},
+		(error) => {
+			console.log(error.response);
+			if (error.response.status === 401) {
+				console.log("AUTH ERROR");
+			}
+			return Promise.reject(error);
+		}
+	);
+
 	const displayAlert = () => {
 		dispatch({ type: DISPLAY_ALERT });
 		clearAlert();
@@ -104,7 +135,12 @@ const AppProvider = ({ children }) => {
 	};
 
 	const updateUser = async (currentUser) => {
-		console.log(currentUser);
+		try {
+			const { data } = await authFetch.patch("/auth/updateUser", currentUser);
+			console.log(data);
+		} catch (error) {
+			//console.log(error.response);
+		}
 	};
 
 	const toggleSidebar = () => {
