caf27f446432cfd91aeb6d5baf4c8dd25f8b4f9c
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-03-03 20:29:42 +0530

    GetAllJobs - Server

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -19,7 +19,10 @@ const deleteJob = async (req, res) => {
 };
 
 const getAllJobs = async (req, res) => {
-	res.send("All jobs");
+	const jobs = await Job.find({ createdBy: req.user.userId });
+	res
+		.status(StatusCodes.OK)
+		.json({ jobs, totalJobs: jobs.length, numOfPages: 1 });
 };
 
 const updateJob = async (req, res) => {
