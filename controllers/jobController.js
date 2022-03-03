7075d2293aaebd4127db44b8ac8087b74b1b87b1
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-03-03 12:06:56 +0530

    Job Model and Create Job Controller

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,5 +1,17 @@
+import { StatusCodes } from "http-status-codes";
+import Job from "../models/Job.js";
+import { BadRequestError, NotFoundError } from "../errors/index.js";
+
 const createJob = async (req, res) => {
-	res.send("Created job");
+	const { position, company } = req.body;
+
+	if (!position || !company) {
+		throw new BadRequestError("Please provide all values");
+	}
+	req.body.createdBy = req.user.userId;
+
+	const job = await Job.create(req.body);
+	res.status(StatusCodes.CREATED).json({ job });
 };
 
 const deleteJob = async (req, res) => {
